361d2363db17da86da7bf1c9639a09fc3350b416
Gupreet Singh <gurpreet.singh@asos.com>
2022-09-15 19:47:43 +0100

    added dotnet version 5 and 6 support

--- GovUkBankHolidayTests/GovDotUkBankHolidayResponseExtensionsShould.cs
+++ GovUkBankHolidayTests/GovDotUkBankHolidayResponseExtensionsShould.cs
@@ -4,25 +4,36 @@ using FluentAssertions;
 using Gov.UK.BankHolidayProvider.Beans;
 using Gov.UK.BankHolidayProvider.Logic;
 using Gov.UK.BankHolidayProvider.Services;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace GovUkBankHolidayTests
 {
     public sealed class GovDotUkBankHolidayResponseExtensionsShould
     {
-        private readonly GovDotUkBankHolidayService _service;
+        private readonly IGovDotUkBankHolidayService _service;
 
         public GovDotUkBankHolidayResponseExtensionsShould()
         {
-            _service=new GovDotUkBankHolidayService();
+            _service=Substitute.For<IGovDotUkBankHolidayService>();
         }
 
-        [TestCase("25 Dec", true)]
-        [TestCase("01 Jan", true)]
+        [TestCase("26 Dec", true)]
         [TestCase("15 Jan", false)]
         public async Task ReturnTrueOrFalseBaseOnWhenGivenDateIsBankHolidayOrNotAsync(string testDateWithoutYear, bool isExpectedBankHoliday)
         {
-            var testDate = DateTime.Parse($"{testDateWithoutYear} {DateTime.Now.Year}");
+            var testDate = DateTime.Parse($"{testDateWithoutYear} {2022}");
+            var expectedResponse = new GovDotUkBankHolidaysResponse
+            {
+                EnglandAndWales = new DivisionEvents
+                {
+                    Division = GovDotUkBankHolidayDivision.EnglandAndWales,
+                    Events = new[] { new BankHolidayEvent { Date= testDate,Title = "NA" } } 
+                }
+            };
+
+            if(isExpectedBankHoliday)  _service.GetBankHolidaysAsync().Returns(expectedResponse);
+
             var response = await _service.GetBankHolidaysAsync();
             var isBankHoliday = response.IsBankHoliday(GovDotUkBankHolidayDivision.EnglandAndWales, testDate);
             isBankHoliday.Should().Be(isExpectedBankHoliday);
